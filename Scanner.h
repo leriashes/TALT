e397e7f6ffdcb0753b323366b1e8c72c1347ab6f
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-10-02 22:15:29 +0700

    Scaner.h

---
+++ Scanner.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "defs.h"
+
+
+class TScanner
+{
+private:
+	IM text;		//исходный текст
+	int uk;			//указатель текущей позиции в исходном тексте
+
+public:
+	void PutUK(int i);
+	int GetUK();
+
+	void PrintError(char*, char*);
+	int Scanner(LEX lex);
+	void GetData(char*);
+
+	TScanner(char*);
+	~TScanner() {}
+};
+
