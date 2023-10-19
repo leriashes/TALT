e81f09fcc1f767885e33b9294614364740077f03
leriashes <valeriya-shevchenko-2002@mail.ru>
2023-10-19 22:46:05 +0700

    V-K

--- Diagram.cpp
+++ Diagram.cpp
@@ -125,7 +125,7 @@ void TDiagram::F()			//функция
 
 	if (type != TMain || type != TIdent)
 	{
-		scan->PrintError("Ожидался идентификатор", lex);
+		scan->PrintError("Ожидалось имя функции", lex);
 	}
 
 	type = scan->Scanner(lex);
@@ -275,6 +275,8 @@ void TDiagram::A()			//оператор
 	}
 }
 
+
+
 void TDiagram::W()			//while
 // 
 //		               -----         -----
@@ -341,6 +343,8 @@ void TDiagram::P()			//Присваивание
 	V();
 }
 
+
+
 void TDiagram::R()			//return
 // 
 //		            ----- 
@@ -381,7 +385,236 @@ void TDiagram::B()			//break
 
 
 
-void TDiagram::V()
+void TDiagram::V()			//Выражение
+//               
+//           ----- == ----
+//        ---|           |---
+//        |  ----- != ----  |
+//       \|/     -----      |
+//--------.------| Z |---------->
+//               -----
+//
 {
-	return;
+	LEX lex;
+	int type;
+	int uk1;
+
+	do
+	{
+		Z();
+		type = scan->Scanner(lex);
+		uk1 = scan->GetUK();
+	} while (type == TEq || type == TNEq);
+
+	scan->PutUK(uk1);
+}
+
+
+
+void TDiagram::Z()			//Сравнение
+//          
+//           ----- < -----   
+//           |           |
+//           ----- > -----   
+//           |           |
+//           ----- <= ----
+//        ---|           |---
+//        |  ----- >= ----  |
+//       \|/     -----      |
+//--------.------| Y |---------->
+//               -----
+//
+{
+	LEX lex;
+	int type;
+	int uk1;
+
+	do
+	{
+		Y();
+		type = scan->Scanner(lex);
+		uk1 = scan->GetUK();
+	} while (type == TLT || type == TGT || type == TLE || type == TGE);
+
+	scan->PutUK(uk1);
+}
+
+
+
+void TDiagram::M()			//Множитель
+//          
+//           ----- * ----   
+//           |          |
+//           ----- / ----
+//        ---|          |---
+//        |  ----- % ----  |
+//       \|/     -----     |
+//--------.------| N |---------->
+//               -----
+//
+{
+	LEX lex;
+	int type;
+	int uk1;
+
+	do
+	{
+		N();
+		type = scan->Scanner(lex);
+		uk1 = scan->GetUK();
+	} while (type == TMult || type == TDiv || type == TMod);
+
+	scan->PutUK(uk1);
+}
+
+
+
+void TDiagram::Y()			//Сдвиг
+//          
+//           ----- << ----
+//        ---|           |---
+//        |  ----- >> ----  |
+//       \|/     -----      |
+//--------.------| L |---------->
+//               -----
+//
+{
+	LEX lex;
+	int type;
+	int uk1;
+
+	do
+	{
+		L();
+		type = scan->Scanner(lex);
+		uk1 = scan->GetUK();
+	} while (type == TLShift || type == TRShift);
+
+	scan->PutUK(uk1);
+}
+
+
+
+void TDiagram::L()			//Слагаемое
+//          
+//           ----- + ----
+//        ---|          |---
+//        |  ----- - ----  |
+//       \|/     -----     |
+//--------.------| M |---------->
+//               -----
+//
+{
+	LEX lex;
+	int type;
+	int uk1;
+
+	do
+	{
+		M();
+		uk1 = scan->GetUK();
+		type = scan->Scanner(lex);
+	} while (type == TPlus || type == TMinus);
+
+	scan->PutUK(uk1);
+}
+
+
+
+void TDiagram::N()			//Со знаком
+//					 --- a -----------------
+//                   |                     |
+//		--- + ---    |-- c1 ---------------|            
+// -----|-------|----|-- c2 ---------------|----->
+//		--- - ---	 |   -----             |
+//					 |---| K |-------------|
+//					 |   -----             |
+//					 |		  -----	       |
+//					 --- ( ---| V |--- ) ---
+//							  -----			 
+//
+{
+	LEX lex;
+	int type;
+	int uk1;
+
+	uk1 = scan->GetUK();
+	type = scan->Scanner(lex);
+
+	if (type != TPlus && type != TMinus)
+	{
+		scan->PutUK(uk1);
+	}
+
+	uk1 = scan->GetUK();
+	type = scan->Scanner(lex);
+
+	if (type == TLS)
+	{
+		V();
+
+		type = scan->Scanner(lex);
+
+		if (type != TRS)
+		{
+			scan->PrintError("Ожидался символ ')'", lex);
+		}
+	}
+	else if (type == TMain)
+	{
+		scan->PutUK(uk1);
+		K();
+	}
+	else if (type == TIdent)
+	{
+		int uk2;
+		
+		uk2 = scan->GetUK();
+		type = scan->Scanner(lex);
+
+		if (type == TLS)
+		{
+			scan->PutUK(uk1);
+			K();
+		}
+	}
+	else if (type != TConstInt && type != TConstFloat)
+	{
+		scan->PrintError("Ожидалось элементарное выражение", lex);
+	}
+}
+
+
+
+void TDiagram::K()			//Вызов функции
+// 
+//		----- a -----                
+// -----|			|--- ( --- ) ----->
+//		---- main ---				 
+//
+{
+	LEX lex;
+	int type;
+	int uk1;
+
+	type = scan->Scanner(lex);
+
+	if (type != TMain || type != TIdent)
+	{
+		scan->PrintError("Ожидалось имя функции", lex);
+	}
+
+	type = scan->Scanner(lex);
+
+	if (type != TLS)
+	{
+		scan->PrintError("Ожидался символ '('", lex);
+	}
+
+	type = scan->Scanner(lex);
+
+	if (type != TRS)
+	{
+		scan->PrintError("Ожидался символ ')'", lex);
+	}
 }
